073fbf556488dec0279717cd71bc2457d5f8ac5b
yash1509 <yash1509@gmail.com>
2017-05-30 15:28:05 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,8 +2,12 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
+    'firebase',
     'myApp.config',
     'myApp.security',
+
+    'firebase.appauth',
+
     'myApp.home',
     'myApp.account',
     'myApp.chat',
@@ -11,6 +15,14 @@ angular.module('myApp', [
   ])
   
   .config(['$routeProvider', function ($routeProvider) {
+    // Initialize Firebase
+    var config = {
+       apiKey: PRIVATE.firebase_api,
+       authDomain: PRIVATE.firebase_authDomain,
+       databaseURL: PRIVATE.firebase_databaseURL
+    };
+    firebase.initializeApp(config);
+
     $routeProvider.otherwise({
       redirectTo: '/home'
     });
@@ -18,7 +30,7 @@ angular.module('myApp', [
   
   .run(['$rootScope', 'Auth', function($rootScope, Auth) {
     // track status of authentication
-    Auth.$onAuth(function(user) {
+    Auth.$onAuthStateChanged(function(user) {
       $rootScope.loggedIn = !!user;
     });
   }]);
