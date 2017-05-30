a242b28d8ffaf8dc34f588e09df958d46c020111
yash1509 <yash1509@gmail.com>
2017-05-30 15:29:21 +0530

    Update auth.js

--- app/components/auth/auth.js
+++ app/components/auth/auth.js
@@ -1,4 +1,6 @@
-angular.module('firebase.auth', ['firebase', 'firebase.utils'])
-  .factory('Auth', ['$firebaseAuth', 'fbutil', function($firebaseAuth, fbutil) {
-    return $firebaseAuth(fbutil.ref());
+angular.module('firebase.appauth', ['firebase'])
+  // renamed as per 
+  // https://stackoverflow.com/questions/39244924/angularfire-and-firebase-upgrade-error-unknown-provider-firebaseauthprovider#39249547
+  .factory('Auth', ['$firebaseAuth', function($firebaseAuth) {
+    return $firebaseAuth();
   }]);
