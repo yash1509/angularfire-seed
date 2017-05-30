7f18fe340749af32b2dcb082180a22a49df605a3
yash1509 <yash1509@gmail.com>
2017-05-30 15:34:45 +0530

    Create private_dummy.js

---
+++ app/private_dummy.js
@@ -0,0 +1,13 @@
+// private-dummy.js
+// contains iDUMMY API keys . . .
+//
+// Use this as a template for private.js with real keys
+//
+// and assume that ithe real private.js is included in .gitingore
+ var PRIVATE  = {
+	 firebase_api: 'DUMMY-FIREBASE-API-KEY-not-real!!!!!!!!',
+	 firebase_authDomain: 'INSTANCE.firebaseapp.com',
+	 firebase_databaseURL: 'https://INSTANCE.firebaseio.com',
+	 storageBucket: "INSTANCE.appspot.com",
+         messagingSenderId: "000000000000"
+ };
